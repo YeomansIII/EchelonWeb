0f21dcca7e4e06f4f0f63ebcf2ca40a4d4bef87c
Jason Yeomans <jyeo098@gmail.com>
2015-09-22 18:31:15 -0400

    add some logic to determine if running locally or not

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,8 +22,12 @@ angular.module('webApp', [
   'spotify'
 ]).config(function(SpotifyProvider) {
   SpotifyProvider.setClientId('8b81e3deddce42c4b0f2972e181b8a3a');
-  //SpotifyProvider.setRedirectUri('http://localhost:9000/spotify-callback.html');
-  SpotifyProvider.setRedirectUri('https://echelonapp.io/spotify-callback.html');
+  var origin = document.location.origin;
+  if (origin.indexOf('localhost') > -1) {
+    SpotifyProvider.setRedirectUri(document.location.origin + '/spotify-callback.html');
+  } else {
+    SpotifyProvider.setRedirectUri('https://echelonapp.io/spotify-callback.html');
+  }
   SpotifyProvider.setScope('user-read-private user-read-email');
 }).config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.useXDomain = true;
