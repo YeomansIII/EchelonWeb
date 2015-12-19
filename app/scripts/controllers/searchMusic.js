390ada6b83cf70c93815d5a905ee8f48adb65359
Jason Yeomans <jyeo098@gmail.com>
2015-12-19 15:21:03 -0500

    add song search

--- app/scripts/controllers/searchMusic.js
+++ app/scripts/controllers/searchMusic.js
@@ -1,3 +1,4 @@
+/*jshint camelcase:false*/
 'use strict';
 /**
  * @ngdoc function
@@ -7,11 +8,27 @@
  * A demo of using AngularFire to manage a synchronized list.
  */
 angular.module('webApp')
-  .controller('SearchMusicCtrl', function($scope, Spotify) {
+  .controller('SearchMusicCtrl', function($scope, Spotify, Firebase) {
     $scope.query = '';
+    $scope.tracks = [];
     $scope.makeSearch = function() {
+      $scope.tracks = [];
       Spotify.search($scope.query, 'track').then(function(data) {
         console.log(data);
+        data.tracks.items.forEach(function(object) {
+          console.log(object);
+          var temp = {
+            added: Firebase.ServerValue.TIMESTAMP,
+            album: object.album.name,
+            albumArtSmall: object.album.images[0].url,
+            artist: object.artists[0].name,
+            lengthMs: object.duration_ms,
+            songId: object.id,
+            title: object.name,
+            uri: object.uri
+          };
+          $scope.tracks.push(temp);
+        });
       });
     };
   });
